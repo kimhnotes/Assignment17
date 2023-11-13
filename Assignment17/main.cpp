36495bd17f3407b6d56204e90573d940610ef07b
Kim Notes <kimhnotes@gmail.com>
2023-11-12 17:08:54 -0800

    done.

--- Assignment17/main.cpp
+++ Assignment17/main.cpp
@@ -6,6 +6,7 @@
 #include <fstream>
 #include "input.h"
 #include <map>
+#include <vector>
 
 using namespace std;
 
@@ -25,9 +26,9 @@ int menuOption()
     cout << "\n\t" << string(60, char(205));
     cout << "\n\t3. Challenge 3 - Capital Quiz";
     cout << "\n\t4. Challenge 4 - File Encryption and Decryption";
-    cout << "\n\t6. Challenge 6 - ";
-    cout << "\n\t7. Challenge 7 - ";
-    cout << "\n\t8. Challenge 8 - ";
+    cout << "\n\t6. Challenge 6 - Word Frequency";
+    cout << "\n\t7. Challenge 7 - Word Index";
+    cout << "\n\t8. Challenge 8 - Prime Number Generation";
     cout << "\n\t" << string(60, char(196));
     cout << "\n\t0. exit";
     cout << "\n\t" << string(60, char(205));
@@ -56,6 +57,10 @@ int main()
 
     return EXIT_SUCCESS;
 }
+
+// Function: Challenge3()
+// Precondition/Purpose: Prompts the user to enter valid file name
+// Postcondition: The capital quiz is displayed
 
 void Challenge3()
 {
@@ -120,6 +125,9 @@ void Challenge3()
     cout << "Incorrect answers: " << incorrects << '\n';
 }
 
+// Function: Challenge4()
+// Precondition/Purpose: Prompts the user to enter valid file name
+// Postcondition: Text from file is encrypted or decrypted and written to another file
 
 void Challenge4()
 {
@@ -223,26 +231,202 @@ void Challenge4()
     cout << "\n\tFile encryption and decryption completed.\n";
 }
 
+// Function: Challenge6()
+// Precondition/Purpose: Prompts the user to enter valid file name
+// Postcondition: Word frequency is displayed or written to another file
+
 void Challenge6()
 {
 	system("cls");
-	cout << "\n\t6. Challenge 6 - ";
+	cout << "\n\t6. Challenge 6 - Word Frequency";
 	cout << "\n\t" << string(60, char(196));
 
+    string fileName = inputString("\n\tEnter the name of the file to read (example file is called \"Kennedy.txt\"): ", true);
+
+    fstream file(fileName, ios::in);
+
+    if (file.fail())
+    {
+		cout << "\n\tERROR: Cannot read " << fileName << ".\n";
+		return;
+	}
+
+    map<string, int> wordFrequency;
+
+	string word;
+
+    while (file >> word)
+    {
+		auto it = wordFrequency.find(word);
+        if (it != wordFrequency.end())
+        {
+			it->second++;
+		}
+        else
+        {
+			wordFrequency.insert(pair<string, int>(word, 1));
+		}
+	}
+
+	file.close();
+
+	char choice = inputChar("\n\tChoose operation (D - Display, W - Write to file): ", "DWdw");
+
+    if (toupper(choice) == 'D')
+    {
+        for (const auto& pair : wordFrequency)
+        {
+			cout << "\n\t" << pair.first << " : " << pair.second;
+		}
+	}
+    else if (toupper(choice) == 'W')
+    {
+		string outputFileName = inputString("\n\tEnter the name of the file to write word frequency (example file is called \"blankForWordFrequency.txt\"): ", true);
+
+		file.open(outputFileName, ios::out);
+
+        if (file.fail())
+        {
+			cout << "\n\tERROR: Cannot write " << outputFileName << ".\n";
+			return;
+		}
+
+        for (const auto& pair : wordFrequency)
+        {
+			file << pair.first << " : " << pair.second << '\n';
+		}
+
+		file.close();
+
+		cout << "\n\tWord frequency written to " << outputFileName << ".\n";
+	}
+
 }
+
+// Function: Challenge7()
+// Precondition/Purpose: Prompts the user to enter valid file name
+// Postcondition: Word index is displayed or written to another file
 
 void Challenge7()
 {
-	system("cls");
-	cout << "\n\t7. Challenge 7 - ";
-	cout << "\n\t" << string(60, char(196));
+    system("cls");
+    cout << "\n\t7. Challenge 7 - Word Index";
+    cout << "\n\t" << string(60, char(196));
 
+    string fileName = inputString("\n\tEnter the name of the file to read (example file is called \"Kennedy.txt\"): ", true);
+
+    fstream file(fileName, ios::in);
+
+    if (file.fail())
+    {
+        cout << "\n\tERROR: Cannot read " << fileName << ".\n";
+        return;
+    }
+
+    map<string, vector<int>> wordIndex;
+
+    string word;
+
+    int line = 1;
+
+    while (file >> word)
+    {
+        auto it = wordIndex.find(word);
+        if (it != wordIndex.end())
+        {
+            it->second.push_back(line);
+        }
+        else
+        {
+            wordIndex.insert(pair<string, vector<int>>(word, { line }));
+        }
+
+        // Check for the end of a line
+        if (file.peek() == '\n' || file.peek() == '\r')
+        {
+            line++;
+        }
+    }
+
+    file.close();
+
+    char choice = inputChar("\n\tChoose operation (D - Display, W - Write to file): ", "DWdw");
+
+    if (toupper(choice) == 'D')
+    {
+        for (const auto& pair : wordIndex)
+        {
+            cout << "\n\t" << pair.first << " : ";
+            for (const auto& line : pair.second)
+            {
+                cout << line << " ";
+            }
+        }
+    }
+    else if (toupper(choice) == 'W')
+    {
+        string outputFileName = inputString("\n\tEnter the name of the file to write word index (example file is called \"index.txt\"): ", true);
+
+        file.open(outputFileName, ios::out);
+
+        if (file.fail())
+        {
+            cout << "\n\tERROR: Cannot write " << outputFileName << ".\n";
+            return;
+        }
+
+        for (const auto& pair : wordIndex)
+        {
+            file << pair.first << " : ";
+            for (const auto& line : pair.second)
+            {
+                file << line << " ";
+            }
+            file << '\n';
+        }
+
+        file.close();
+
+        cout << "\n\tWord index written to " << outputFileName << ".\n";
+    }
 }
+
+// Function: Challenge8()
+// Precondition/Purpose: Prompts the user to enter a positive integer greater than 1
+// Postcondition: Prime numbers up to the number entered are displayed
 
 void Challenge8()
 {
 	system("cls");
-	cout << "\n\t8. Challenge 8 - ";
+	cout << "\n\t8. Challenge 8 - Prime Number Generation";
 	cout << "\n\t" << string(60, char(196));
 
+    int number = inputInteger("\n\tEnter a positive integer greater than 1: ", 2);
+
+    vector<int> primes;
+
+    for (int i = 2; i <= number; i++)
+    {
+		bool isPrime = true;
+        for (int j = 2; j < i; j++)
+        {
+            if (i % j == 0)
+            {
+				isPrime = false;
+			}
+		}
+        if (isPrime)
+        {
+			primes.push_back(i);
+		}
+	}
+
+    cout << "\n\tPrime numbers smaller than or equal to " << number << " are: ";
+
+    for (const auto& prime : primes)
+    {
+		cout << prime << " ";
+	}
+
+    cout << "\n";
 }
